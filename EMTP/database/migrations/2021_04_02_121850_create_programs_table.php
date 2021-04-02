9f6401dd533ed275da5782557acf2e8d1efce336
ben1234456 <benjsl1211@gmail.com>
2021-04-02 21:21:47 +0800

    Add database table for client training program

--- EMTP/database/migrations/2021_04_02_121850_create_programs_table.php
+++ EMTP/database/migrations/2021_04_02_121850_create_programs_table.php
@@ -17,6 +17,7 @@ class CreateProgramsTable extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('type');
+            $table->decimal('price', $precision = 4, $scale = 2);
             $table->enum('option',['physical','online','both']);
             $table->text('description');
             $table->timestamps();
---
+++ EMTP/database/migrations/2021_04_02_130016_create_client_programs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_programs', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_email');
+            $table->string('company_name');
+            $table->string('program_name');
+            $table->string('client_venue');
+            $table->integer('no_of_employees');
+            $table->enum('payment_type',['cash','online banking','credit/debit card']);
+            $table->enum('payment_status',['pending','approved']);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->text('client_notes');
+            $table->enum('status',['pending','to-be-confirmed','approved','rejected']);
+            $table->timestamps();
+        });
+        
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_programs');
+    }
+}
