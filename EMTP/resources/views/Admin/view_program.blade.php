7b5fc9a19dad864e66f9b21d839b2691e07269e0
ben1234456 <benjsl1211@gmail.com>
2021-04-05 20:52:17 +0800

    Admin approves program

--- EMTP/resources/views/Admin/view_program.blade.php
+++ EMTP/resources/views/Admin/view_program.blade.php
@@ -2,23 +2,31 @@
 @section('title', 'Admin View Program')
 @section('content')
 
-<h1>Program List</h1>
+<h1>Pending Program List</h1>
 
 <table class="table table-striped">
-    <thead>
-        <tr>
-            <th>Name</th>
-        </tr>
-    </thead>
     <tbody>
-    @foreach($programs as $program)
+    @foreach($pendingprograms as $program)
     <tr>
-        <td><a href="{{$program->id}}">{{$program->name}}</a> </td>
+        <td><a class="hover:bg-blue-700" href="programs/{{$program->id}}">{{$program->name}}</a> </td>
     </tr>
     @endforeach
     </tbody>
 </table>
-<hr>
+
+<div class="my-8">
+    <h1>Program List</h1>
+    <table class="table table-striped">
+    <tbody>
+    @foreach($approvedprograms as $program)
+    <tr>
+        <td><a class="hover:bg-blue-700" href="programs/edit/{{$program->id}}">{{$program->name}}</a> </td>
+    </tr>
+    @endforeach
+    </tbody>
+    </table>
+</div>
+
 {{-- <a href="cars/create">Add New Car</a> --}}
 
 @endsection
