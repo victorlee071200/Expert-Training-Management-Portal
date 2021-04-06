86653f81ea015fc6338dd3f59892d4408811c6b7
Lai Kok Wui <kokwei325@gmail.com>
2021-04-06 12:56:20 +0800

    remove navigation bar

--- EMTP/resources/views/dashboard.blade.php
+++ EMTP/resources/views/dashboard.blade.php
@@ -1,48 +1,5 @@
 <x-app-layout>
     <x-slot name="header">
-        <!--Navigation bar design-->
-        <nav
-            class="flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow">
-            <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
-                <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                    <span class="font-semibold text-xl tracking-tight">EMTP</span>
-                </div>
-                <div class="block lg:hidden ">
-                    <button
-                        id="nav"
-                        class="flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700">
-                        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <!--navigation bar list-->
-            <div class="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8">
-                <div class="text-md font-bold text-blue-700 lg:flex-grow">
-                    <a href="#responsive-header"
-                    class="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                        Home
-                    </a>
-                    <a href="#responsive-header"
-                    class=" block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                        Programs
-                    </a>
-                    <a href="{{ __('dashboard') }}"
-                    class=" block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                        Dashboard
-                    </a>
-                    <a href="#responsive-header"
-                    class="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                        About Us
-                    </a>
-                    <a href="#responsive-header"
-                    class="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2">
-                        Support
-                    </a>
-                </div>
-            </div>
-        </nav>
         <!--Dashboard title-->
         <h1 class="font-semibold text-3xl text-gray-800 leading-tight mt-7 ml-3">Dashboard</h1>
         <!--content-->
