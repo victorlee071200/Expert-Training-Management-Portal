c163f89969473a24cc3d4ec66b987e567e3aa474
Lai Kok Wui <kokwei325@gmail.com>
2021-04-08 17:39:35 +0800

    Added homepage design
    need more ideas

--- EMTP/resources/views/client/homepage/index.blade.php
+++ EMTP/resources/views/client/homepage/index.blade.php
@@ -1,17 +1,41 @@
-<x-app-layout title="Home">
+<x-app-layout title="EMTP - Homepage">
+    <div class="bg-cover bg-left md:bg-left h-auto text-white py-24 px-10 object-left-top" style="background-image: url(https://www.denny.co.uk/wp-content/uploads/2016/04/in-house-training-banner.jpg)">
+        <div class="ml-5 md:ml-14 xl:ml-24 2xl:ml-32 md:w-1/2">
+            <p class="font-bold text-sm uppercase text-black">Services</p>
+            <p class="text-3xl md:text-4xl font-bold text-black">Training Programs</p>
+            <p class="text-2xl md:text-3xl mb-10 leading-none text-black">Train for your future</p>
+            <a href="#" class="bg-purple-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">Register Now</a>
+        </div>  
+    </div>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                
+    <div class="mx-5 md:mx-10 xl:mx-24">
+        <h1 class="text-3xl md:text-4xl font-bold text-center m-5">Traing Program Catergories</h1>
+        <!--Small Cards list-->
+        <div class="grid grid-cols-2 md:grid-cols-4 bg-white">
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Management</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Software</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Creative</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Jobs</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Communication</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Fitness</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Skills</p>
+            </div>
+            <div class="hover:bg-gray-100 mx-2 my-2 px-2 py-2 border-black border-2 text-center">
+                <p>Lifestyle</p>
             </div>
         </div>
     </div>
-
 </x-app-layout>
