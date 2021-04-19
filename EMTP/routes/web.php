369aa3739f5015ee03319f1ef467a9b62429ae86
victorlee071200 <victorlee071200@gmail.com>
2021-04-19 20:59:06 +0800

    Update web.php

---
+++ EMTP/routes/web.php
@@ -0,0 +1,126 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\SupportController;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AdminProgramController;
+use App\Http\Controllers\ClientProgramController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+    // Client Side
+
+    // Dashboard Page
+    Route::get('/client/dashboard', [DashboardController::class, 'ClientDashboard'])->name('client-dashboard');
+
+    // Home Page
+    Route::get('/home', [HomepageController::class, 'index'])->name('client-home');
+
+    // Program Page
+    //View all approved programs
+    Route::get('/client/view/program', [ProgramController::class, 'index'])->name('client-program');
+
+    //View Specific Program
+    Route::get('/client/view/program/{program}', [ProgramController::class, 'ClientViewSpecificProgram']);
+
+    //Register a program
+    Route::get('/client/view/program/{program}/register', [ProgramController::class, 'ClientRegisterProgram']);
+
+    //Get registration details from user and save to database
+    Route::post('/client/view/program/{program}/register', [ProgramController::class, 'ClientStoreProgram']);
+
+    //View all registered programs
+    Route::get('/client/view/registered', [ProgramController::class, 'ClientViewRegisteredProgram']);
+
+    //View specific registered program details
+    Route::get('/client/registered/{registeredprogram}/{program}', [ProgramController::class, 'ClientViewSpecificRegisteredProgram']);
+
+    //About Us Page
+    Route::get('/about-us', [AboutUsController::class, 'index'])->name('about-us');
+
+    //Support & Help Page
+    Route::get('/support', [SupportController::class, 'index'])->name('support');
+});
+
+//Staff routes
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+    //Program Module
+    //Staff Dashboard
+    Route::get('/staff/dashboard', [DashboardController::class, 'StaffDashboard'])->name('staff-dashboard');
+
+    //Create a program
+    Route::get('/staff/create/program', [ProgramController::class, 'StaffCreateProgram'])->name('staff-create-program');
+
+    //Register a program with button
+    Route::post('/staff/create/program', [ProgramController::class, 'StaffRegisterProgram'])->name('staff-register-program');
+
+    //View all pending programs
+    Route::get('/staff/view/pendings', [ProgramController::class, 'StaffViewPendingProgram'])->name('staff-view-program');
+});
+
+// Admin Routes
+// Route::middleware(['auth:sanctum', 'verified','auth.admin'])->group(function () {
+
+//Program Module
+//Dashboard
+Route::get('/admin/dashboard', [DashboardController::class, 'AdminDashboard'])->name('admin-dashboard');
+// View
+Route::get('/admin/view/program', [ProgramController::class, 'AdminShowAllPrograms'])->name('admin-view-all-programs');
+
+//View Specific Program
+Route::get('/admin/view/program/{program}', [ProgramController::class, 'AdminViewSpecificProgram'])->name('admin-view-specific-program');
+
+//Approve Specific Program
+Route::put('/admin/view/program/{program}', [ProgramController::class, 'AdminApprovedProgram'])->name('admin-approve-a-program');
+
+// View Specific Approved Program
+Route::get('/admin/view/approved/program/{program}', [ProgramController::class, 'AdminViewApprovedProgram'])->name('admin-view-specific-approved-program');
+
+
+// });
+
+
+// Route::put('/admin/programs/pending/{program}', [AdminProgramController::class, 'update']);
+
+// Route::put('/admin/programs/approved/{program}', [AdminProgramController::class, 'edit']);
+
+// route to Training Requisite Process
+Route::get('/selection', function () {
+    return view('client.requisite_process.selection');
+});
+
+Route::get('/payment_method', function () {
+    return view('client.requisite_process.payment_method');
+});
+
+Route::get('/payment_credit_card', function () {
+    return view('client.requisite_process.payment_credit_card');
+});
+
+Route::get('/payment_result', function () {
+    return view('client.requisite_process.payment_result');
+});
+
+Route::get('/help_support', function () {
+    return view('help-support-page');
+});
