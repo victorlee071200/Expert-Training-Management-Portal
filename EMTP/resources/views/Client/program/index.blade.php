851ff028bfdfea8353c5503c7ec2ba0d3b0811fe
Lai Kok Wui <kokwei325@gmail.com>
2021-04-20 22:40:14 +0800

    Remove a lot of div tag from program detail page

--- EMTP/resources/views/Client/program/index.blade.php
+++ EMTP/resources/views/Client/program/index.blade.php
@@ -1,52 +1,65 @@
 <x-app-layout title="Program">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+    <div>
+        <div class="bg-white sm:rounded-lg">
             {{-- put your content here --}}
             <div class="grid md:grid-cols-3">
                 <!-- content wrapper-->
-                <div class="md:col-span-1 md:flex md:justify-end">
+                <div class="flex md:justify-end">
                     <nav class="text-right">
                         <div class="flex justify-between items-center">
                             <h1 class="font-bold uppercase p-4 border-b border-gray-100">
                                 <a href="/" class="hover:text-gray-700">Training Module Page</a>
                             </h1>
-                            <div class="px-4 cursor-pointer md:hidden" id="training">
-                                <svg class="w-6" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16" />
+                            <div class="px-4 cursor-pointer md:hidden">
+                                <svg class="w-6" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                                 </svg>
                             </div>
                         </div>
-                        <ul class="text-sm mt-6 hidden md:block" id="menu">
+                        <ul class="text-sm mt-6 hidden md:block">
                             <li class="text-gray-700 font-bold py-1">
                                 <a href="#" class="px-4 flex justify-end border-r-4 border-primary">
-                                    <span>Home</span>
-                                    <svg class="w-5 ml-2" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                                    </svg>
+                                    <span>Latest</span>
                                 </a>
                             </li>
                             <li class="py-1">
                                 <a href="#" class="px-4 flex justify-end border-r-4 border-white">
-                                    <span>About</span>
-                                    <svg class="w-5 ml-2" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
+                                    <span>Management</span>
                                 </a>
                             </li>
                             <li class="py-1">
                                 <a href="#" class="px-4 flex justify-end border-r-4 border-white">
-                                    <span>Contact</span>
-                                    <svg class="w-5 ml-2" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                                    </svg>
+                                    <span>Software</span>
+                                </a>
+                            </li>
+                            <li class="py-1">
+                                <a href="#" class="px-4 flex justify-end border-r-4 border-white">
+                                    <span>Creative</span>
+                                </a>
+                            </li>
+                            <li class="py-1">
+                                <a href="#" class="px-4 flex justify-end border-r-4 border-white">
+                                    <span>Jobs</span>
+                                </a>
+                            </li>
+                            <li class="py-1">
+                                <a href="#" class="px-4 flex justify-end border-r-4 border-white">
+                                    <span>Communication</span>
+                                </a>
+                            </li>
+                            <li class="py-1">
+                                <a href="#" class="px-4 flex justify-end border-r-4 border-white">
+                                    <span>Fitness</span>
+                                </a>
+                            </li>
+                            <li class="py-1">
+                                <a href="#" class="px-4 flex justify-end border-r-4 border-white">
+                                    <span>Skills</span>
+                                </a>
+                            </li>
+                            <li class="py-1">
+                                <a href="#" class="px-4 flex justify-end border-r-4 border-white">
+                                    <span>Lifestyle</span>
                                 </a>
                             </li>
                         </ul>
@@ -55,18 +68,8 @@
                 <!--end nav-->
 
                 <main class="px-16 py-6 bg-gray-100 md:col-span-2">
-                    <div class="flex justify-center md:justify-end">
-                        <a href="#"
-                            class="btn text-primary border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500">Log
-                            in</a>
-                        <a href="#"
-                            class="btn text-primary ml-2 border-primary md:border-2 hover:bg-primary hover:text-white transition ease-out duration-500">Sign
-                            up</a>
-                    </div>
-
                     <header>
                         <h2 class="text-gray-700 text-6xl font-semibold">Training Programmes</h2>
-                        <h3 class="text-2xl font-semibold">For Clients</h3>
                     </header>
 
                     <!--Search bar-->
@@ -106,7 +109,6 @@
                         </a>
                         @endforeach
 
-
                         <h4 class="font-bold mt-12 pb-2 border-b border-gray-200">Most Popular</h4>
 
                         <div class="mt-8 grid lg:grid-cols-3 gap-10">
@@ -133,11 +135,8 @@
                                 Load more</div>
                         </div>
                     </div>
-
                 </main>
             </div>
-
-
         </div>
     </div>
 </x-app-layout>
--- EMTP/resources/views/client/program/detail.blade.php
+++ EMTP/resources/views/client/program/detail.blade.php
@@ -1,150 +1,59 @@
 <x-app-layout title="Registered Program Detail">
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Client | View Registered Program Detail') }}
-    </h2>
-  </x-slot>
-  <div class="h-screen flex bg-gray-200">
-    <!-- container -->
-    <aside class="flex flex-col items-center bg-white text-gray-700 shadow h-full">
-      <!-- Side Nav Bar-->
+  <div class="bg-white sm:rounded-lg flex">
+    <!-- side nav -->
+    <div class="bg-gray-300 text-gray-700 shadow flex">
       <ul>
-        <!-- Items Section -->
-        <li class="hover:bg-gray-100">
-          <a href="." class="h-16 px-6 flex justify-center items-center w-full focus:text-orange-500">Announcement</a>
+        <li class="hover:bg-white px-10">
+          <a href="" class="h-16 px-6 flex justify-center items-center w-auto focus:text-orange-500 focus:bg-white">Details</a>
         </li>
-
-        <li class="hover:bg-gray-100">
-          <a href="." class="h-16 px-6 flex justify-center items-center w-full focus:text-orange-500">Details</a>
+        <li class="hover:bg-white px-10">
+          <a href="." class="h-16 px-6 flex justify-center items-center w-auto focus:text-orange-500 focus:bg-white">Announcement</a>
         </li>
-
-        <li class="hover:bg-gray-100">
-          <a href="." class="h-16 px-6 flex justify-center items-center w-full focus:text-orange-500">Materials</a>
+        <li class="hover:bg-white px-10">
+          <a href="." class="h-16 px-6 flex justify-center items-center w-auto focus:text-orange-500 focus:bg-white">Materials</a>
         </li>
-
-        <li class="hover:bg-gray-100">
-          <a href="." class="h-16 px-6 flex justify-center items-center w-full focus:text-orange-500">Feedback</a>
+        <li class="hover:bg-white px-10">
+          <a href="." class="h-16 px-6 flex justify-center items-center w-auto focus:text-orange-500 focus:bg-white">Feedback</a>
         </li>
       </ul>
-    </aside>
-
-    <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <section class="text-gray-700 body-font overflow-hidden bg-white">
-              <div class="container px-5 py-24 mx-auto">
-                <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                  <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src = "{{ asset('storage/program_thumbnails/'.$program->thumbnail_path)}}" width="500" height="600"> 
-                  <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Code</h2>
-                      <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$program->code}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Name</h2>
-                      <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$program->name}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Option</h2>
-                      <h1 class="capitalize text-gray-900 text-2xl title-font font-medium mb-1">{{$program->option}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Type</h2>
-                      <h1 class="text-gray-900 text-2xl title-font font-medium mb-1">{{$program->type}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-xs title-font text-gray-500 tracking-widest">Description</h2>
-                      <p class="leading-relaxed">{{ $program->description }}</p>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-xs title-font text-gray-500 tracking-widest">Price</h2>
-                      <div class="flex">
-                        
-                        <span class="title-font font-medium text-2xl text-gray-900">${{ $program->price }}</span>
-                      </div>
-                    </div>                      
-
-                    {{-- <div class="m-6">
-                      <form class="form-horizontal" method="post">
-                        @csrf
-                        @method('put')
-                        <!-- Button -->
-                        <div class="form-group">
-                            <label class="col-md-4 control-label" for="submit"></label>
-                            <div class="col-md-4">
-                              <button id="submit" name="submit" class="block tracking-widest uppercase text-center shadow bg-green-400 hover:bg-green-500 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">Approve</button>
-                            </div>
-                        </div>
-                      </form>
-                    </div> --}}
-                  </div>
-
-                  <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Email</h2>
-                      <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$registeredprogram->client_email}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Company Name</h2>
-                      <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$registeredprogram->company_name}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Number of employees</h2>
-                      <h1 class="capitalize text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->no_of_employees}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Mode</h2>
-                      <h1 class="capitalize text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->option}}</h1>
-                    </div>
-                    
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Payment Type</h2>
-                      <h1 class="capitalize text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->payment_type}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Payment Status</h2>
-                      <h1 class="capitalize text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->payment_status}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Start Date</h2>
-                      <h1 class="text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->start_date}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">End Date</h2>
-                      <h1 class="text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->end_date}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Notes</h2>
-                      <h1 class="text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->client_notes}}</h1>
-                    </div>
-
-                    <div class="m-6">
-                      <h2 class="text-sm title-font text-gray-500 tracking-widest">Status:</h2>
-                      <h1 class="capitalize text-gray-900 text-2xl title-font font-medium mb-1">{{$registeredprogram->status}}</h1>
-                    </div>
-                  
-                  </div>
-
-                </div>
-              </div>
-            </section>
-            </div>     
-          </div>
+    </div>
+    <div class="text-gray-700 body-font bg-white px-5 py-5">
+      <h2 class="text-4xl font-bold text-gray-800 border-b border-gray-100 w-auto">
+        {{ __('Program Detail') }}
+      </h2>
+      <img alt="ecommerce" class="rounded border border-gray-200 m-10" src="{{ asset('storage/program_thumbnails/'.$program->thumbnail_path)}}" width="500" height="600">
+      <div class="title-font text-2xl text-gray-900">
+        <p>Code: {{$program->code}}</p>
+        <p>Name: {{$program->name}}</p>
+        <p>Option: {{$program->option}}</p>
+        <p>Type: {{$program->type}}</p>
+        <p>Description: {{ $program->description }}</p>
+        <p>Price: ${{ $program->price }}</p>
+        <p>Email: {{$registeredprogram->client_email}}</p>
+        <p>Company Name: {{$registeredprogram->company_name}}</p>
+        <p>Number of employees: {{$registeredprogram->no_of_employees}}</p>
+        <p>Mode: {{$registeredprogram->option}}</p>
+        <p>Payment Type: {{$registeredprogram->payment_type}}</p>
+        <p>Payment Status: {{$registeredprogram->payment_status}}</p>
+        <p>Start Date: {{$registeredprogram->start_date}}</p>
+        <p>End Date: {{$registeredprogram->end_date}}</p>
+        <p>Notes: {{$registeredprogram->client_notes}}</p>
+        <p>Status: {{$registeredprogram->status}}</p>
       </div>
+    </div>
   </div>
-
 </x-app-layout>
+
+{{-- <div class="m-6">
+  <form class="form-horizontal" method="post">
+    @csrf
+    @method('put')
+    <!-- Button -->
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="submit"></label>
+        <div class="col-md-4">
+          <button id="submit" name="submit" class="block tracking-widest uppercase text-center shadow bg-green-400 hover:bg-green-500 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">Approve</button>
+        </div>
+    </div>
+  </form>
+</div> --}}
