9b6bdc793a361bfb7ad962ad18591ef4ea947f7d
victorlee071200 <victorlee071200@gmail.com>
2021-05-12 23:51:08 +0800

    Fixing JQuery Issue

--- EMTP/resources/views/layouts/app.blade.php
+++ EMTP/resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
@@ -187,4 +189,5 @@
 </body>
 
 @include('sweetalert::alert')
+@stack('js')
 </html>
