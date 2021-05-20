36a851381616bee982d9947a1026381788be4705
IAN MARCELLUS ONG HONG <ian11ong90@gmail.com>
2021-05-20 17:05:35 +0800

    updated about us, create support ticket

--- EMTP/resources/views/Client/new/support/index.blade.php
+++ EMTP/resources/views/Client/new/support/index.blade.php
@@ -31,15 +31,13 @@
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
                                     <h4 :class="{'text-blue-600 font-bold' : isOpen == true}">Popular articles</h4>
                                     <svg class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                        stroke="currentColor">
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
                                 <div x-show="isOpen" @click.away="isOpen = false" class="mt-3"
                                     :class="{'text-gray-600' : isOpen == true}">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ex. Expedita sunt
-                                    enim, vel amet cumque nulla illum harum. Similique!
+                                    Here are some articles to help you in answering your problems
                                 </div>
                             </div>
 
@@ -48,15 +46,14 @@
                                     <h4 :class="{'text-blue-600 font-bold' : isOpen == true}">Fix problems & request
                                         removals</h4>
                                     <svg class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                        stroke="currentColor">
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
                                 <div x-show="isOpen" @click.away="isOpen = false" class="mt-3"
                                     :class="{'text-gray-600' : isOpen == true}">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ex. Expedita sunt
-                                    enim, vel amet cumque nulla illum harum. Similique!
+                                    Have problems regarding the programmes chosen or want to chose? Click the support ticket
+                                    icon to send inquiries.
                                 </div>
                             </div>
 
@@ -64,15 +61,13 @@
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
                                     <h4 :class="{'text-blue-600 font-bold' : isOpen == true}">Browse the web</h4>
                                     <svg class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                        stroke="currentColor">
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
                                 <div x-show="isOpen" @click.away="isOpen = false" class="mt-3"
                                     :class="{'text-gray-600' : isOpen == true}">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ex. Expedita sunt
-                                    enim, vel amet cumque nulla illum harum. Similique!
+                                    Here are some websites to help you find a solution to your problems.
                                 </div>
                             </div>
 
@@ -81,42 +76,32 @@
                                     <h4 :class="{'text-blue-600 font-bold' : isOpen == true}">Search on your phone or
                                         tablet</h4>
                                     <svg class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                        stroke="currentColor">
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
                                 <div x-show="isOpen" @click.away="isOpen = false" class="mt-3"
                                     :class="{'text-gray-600' : isOpen == true}">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat, ex. Expedita sunt
-                                    enim, vel amet cumque nulla illum harum. Similique!
+                                    You can also get help using your phone or tablet.
                                 </div>
                             </div>
                         </div>
 
                         <div class="main-images mb-8  ">
                             <div class="images md:grid-cols-3 gap-8">
-                                <div class="image bg-white rounded-lg shadow-lg overflow-hidden">
+                                <div class="image bg-gray-400 rounded-lg shadow-lg overflow-hidden">
                                     <a href="{{ route('client.support.create') }}">
-                                        <img src="https://images.unsplash.com/photo-1570447926106-8888f858b0bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+                                        <img src="https://www.pngitem.com/pimgs/m/112-1124462_support-ticket-icon-png-transparent-png-png-download.png"
                                             alt="Send a support ticket" title="Send a support ticket">
-                                        <span class="text-center p-2 text-gray-700 text-sm inline-block w-full">Send a
+                                        <span class="text-center p-2 text-black-700 text-sm inline-block w-full">Send a
                                             support ticket</span>
                                     </a>
                                 </div>
                             </div>
                         </div>
-
-                        <div>
-                            <div class="py-12">
-                                <h2>Please fill in this form to send us questions if you need any inquiries. You will be
-                                notified in three to five business days.</h2>
-                            </div>
-                        </div>
-
                     </div>
-                </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
